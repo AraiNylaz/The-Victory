3a0401fb0e81269bd941d0ea13c0cd4410cd6b77
Roopkotha Studio <roopkotha7d@gmail.com>
2019-11-05 02:10:27 +0600

    Updated chase functions and added death animation

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
@@ -21,6 +22,7 @@ public class EnemyController : MonoBehaviour
     private new Animation animation;
     private NavMeshAgent navMeshAgent;
     private AudioSource audioSource;
+    private EnemyHealth enemyHealth;
     private bool chasing = false;
     private bool firing = false;
     private PlayerController player;
@@ -32,75 +34,89 @@ public class EnemyController : MonoBehaviour
         animation = GetComponent<Animation>();
         navMeshAgent = GetComponent<NavMeshAgent>();
         audioSource = GetComponent<AudioSource>();
+        enemyHealth = GetComponent<EnemyHealth>();
         if (!sightPoint) sightPoint = transform;
     }
 
     void Update()
     {
-        getPlayer(false);
-        if (player)
+        if (!enemyHealth.dead)
         {
-            if (!player.dead)
+            getPlayer(false);
+            if (player)
             {
-                if (!chasing)
+                navMeshAgent.enabled = true;
+                if (!player.dead)
                 {
-                    if (Vector3.Distance(sightPoint.position, player.transform.position) <= aggroDistance)
+                    if (!chasing)
                     {
-                        Vector3 playerPosition = (player.transform.position - sightPoint.position).normalized;
-                        if (Vector3.Dot(transform.forward, playerPosition) > 0 && Vector3.Angle(transform.forward, playerPosition) <= fieldOfView)
-                        {   
-                            Ray ray = new Ray(sightPoint.position, player.transform.position - sightPoint.position);
-                            if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.CompareTag("Player")) chasing = true;
-                        }
-                    }
-                    if (Vector3.Distance(sightPoint.position, player.transform.position) <= 3) chasing = true;
-                } else
-                {
-                    if (Vector3.Distance(sightPoint.position, player.transform.position) <= attackDistance)
-                    {
-                        Ray ray = new Ray(sightPoint.position, transform.forward * attackDistance);
-                        if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.CompareTag("Player"))
+                        if (Vector3.Distance(sightPoint.position, player.transform.position) <= 3) chasing = true;
+                        if (Vector3.Distance(sightPoint.position, player.transform.position) <= aggroDistance)
                         {
-                            firing = true;
+                            Vector3 playerPosition = (player.transform.position - sightPoint.position).normalized;
+                            if (Vector3.Dot(transform.forward, playerPosition) > 0 && Vector3.Angle(transform.forward, playerPosition) <= fieldOfView)
+                            {   
+                                Ray ray = new Ray(sightPoint.position, player.transform.position - sightPoint.position);
+                                if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.CompareTag("Player")) chasing = true;
+                            }
+                        }
+                    } else
+                    {
+                        if (Vector3.Distance(sightPoint.position, player.transform.position) <= attackDistance)
+                        {
+                            Ray ray = new Ray(sightPoint.position, transform.forward * attackDistance);
+                            if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.CompareTag("Player"))
+                            {
+                                firing = true;
+                            } else
+                            {
+                                firing = false;
+                            }
                         } else
                         {
                             firing = false;
                         }
-                    } else
-                    {
-                        firing = false;
                     }
-                }
-                if (firing && Time.time >= nextShot)
+                    if (firing && Time.time >= nextShot)
+                    {
+                        nextShot = Time.time + 60 / RPM;
+                        player.takeDamage(Random.Range(damage.x, damage.y));
+                        if (muzzleLight)
+                        {
+                            muzzleLight.enabled = true;
+                            CancelInvoke("resetEffects");
+                            Invoke("resetEffects", 0.075f);
+                        }
+                        foreach (EnemyController enemyController in FindObjectsOfType<EnemyController>())
+                        {
+                            if (Vector3.Distance(transform.position, enemyController.transform.position) <= 50) enemyController.getPlayer(true);
+                        }
+                        if (audioSource && fireSound) audioSource.PlayOneShot(fireSound);
+                    }
+                } else
                 {
-                    nextShot = Time.time + 60 / RPM;
-                    player.takeDamage(Random.Range(damage.x, damage.y));
-                    if (muzzleLight)
-                    {
-                        muzzleLight.enabled = true;
-                        CancelInvoke("resetEffects");
-                        Invoke("resetEffects", 0.075f);
-                    }
-                    if (audioSource && fireSound) audioSource.PlayOneShot(fireSound);
+                    chasing = false;
+                    firing = false;
                 }
             } else
             {
                 chasing = false;
                 firing = false;
             }
-        } else
-        {
-            chasing = false;
-            firing = false;
-        }
-        if (chasing)
-        {
-            if (!firing)
+            if (chasing)
             {
-                navMeshAgent.isStopped = false;
-                navMeshAgent.SetDestination(player.transform.position);
-                if (animator) animator.SetBool("Walking", true);
-                if (animation && !animation.isPlaying) animation.Play();
+                if (!firing)
+                {
+                    navMeshAgent.isStopped = false;
+                    navMeshAgent.SetDestination(player.transform.position);
+                    if (animator) animator.SetBool("Walking", true);
+                    if (animation && !animation.isPlaying) animation.Play();
+                } else
+                {
+                    navMeshAgent.isStopped = true;
+                    if (animator) animator.SetBool("Walking", false);
+                    if (animation && animation.isPlaying) animation.Stop();
+                }
             } else
             {
                 navMeshAgent.isStopped = true;
@@ -109,12 +125,13 @@ public class EnemyController : MonoBehaviour
             }
         } else
         {
-            navMeshAgent.isStopped = true;
+            navMeshAgent.enabled = false;
             if (animator) animator.SetBool("Walking", false);
             if (animation && animation.isPlaying) animation.Stop();
         }
     }
 
+    #region Main Functions
     public void getPlayer(bool hit)
     {
         player = FindObjectOfType<PlayerController>();
@@ -125,4 +142,30 @@ public class EnemyController : MonoBehaviour
     {
         if (muzzleLight) muzzleLight.enabled = false;
     }
+
+    public void playDeathAnimation()
+    {
+        if (animator)
+        {
+            animator.SetBool("Walking", false);
+            if (Random.value <= 0.5f)
+            {
+                animator.SetTrigger("DieForward");
+            } else
+            {
+                animator.SetTrigger("DieBack");
+            }
+            StartCoroutine(dropBody());
+        }
+    }
+
+    IEnumerator dropBody() //
+    {
+        for (int i = 0; i < 18; i ++)
+        {
+            transform.position -= new Vector3(0, 0.05f, 0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    #endregion
 }
