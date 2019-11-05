6c1c45baa15ac6a712f4bf7573a212bebc95a4fc
Roopkotha Studio <roopkotha7d@gmail.com>
2019-11-06 01:00:49 +0600

    Create GameController.cs

---
+++ Assets/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    [SerializeField] private Text mainText = null;
+
+    [HideInInspector] public bool gameOver = false;
+    [HideInInspector] public bool won = false;
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        gameOver = false;
+        won = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && !won)
+        {
+            mainText.text = "Game Over!";
+        } else if (!gameOver && won)
+        {
+            mainText.text = "Mission Completed!";
+        }
+    }
+}
