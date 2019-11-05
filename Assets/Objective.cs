7110caa4bade2ba5b54f1d5f8840d0a0577ee684
Roopkotha Studio <roopkotha7d@gmail.com>
2019-11-06 02:10:23 +0600

    Update Objective.cs

--- Assets/Objective.cs
+++ Assets/Objective.cs
@@ -37,13 +37,6 @@ public class Objective : MonoBehaviour
                 EnemyHealth enemyHealth = enemies[i].GetComponent<EnemyHealth>();
                 if (enemyHealth && enemyHealth.dead) ++amount;
             }
-            if (amount < enemies.Length)
-            {
-                checkmark.enabled = false;
-            } else
-            {
-                checkmark.enabled = true;
-            }
         } else if (type == "Rescue")
         {
             int amount = 0;
@@ -52,25 +45,35 @@ public class Objective : MonoBehaviour
                 if (hostage.rescued) ++amount;
             }
             hostagesRescued = amount;
-            if (amount < hostages)
-            {
-                checkmark.enabled = false;
-            } else if (amount >= hostages)
-            {
-                checkmark.enabled = true;
-            }
             count.text = "Hostages: " + hostagesRescued + "/" + hostages;
         }
     }
 
     public void isCompleted()
     {
-        if (type == "Clear")
+        if (!completed)
         {
-            if (FindObjectsOfType<EnemyController>().Length <= 0) completed = true;
-        } else if (type == "Rescue")
-        {
-            if (hostagesRescued >= hostages) completed = true;
+            if (type == "Clear")
+            {
+                int amount = 0;
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    EnemyHealth enemyHealth = enemies[i].GetComponent<EnemyHealth>();
+                    if (enemyHealth && enemyHealth.dead) ++amount;
+                }
+                if (amount >= enemies.Length)
+                {
+                    completed = true;
+                    checkmark.enabled = true;
+                }
+            } else if (type == "Rescue")
+            {
+                if (hostagesRescued >= hostages)
+                {
+                    completed = true;
+                    checkmark.enabled = true;
+                }
+            }
         }
     }
 }
