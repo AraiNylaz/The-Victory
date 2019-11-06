686f8f8606c5fd2c4984f7a1e24267318ae223c9
Roopkotha Studio <roopkotha7d@gmail.com>
2019-11-07 00:29:09 +0600

    Update QualityChanger.cs

--- Assets/Scripts/Core/QualityChanger.cs
+++ Assets/Scripts/Core/QualityChanger.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class QualityChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private AudioClip buttonClick = null;
+
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource) audioSource.ignoreListenerPause = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void changeQuality(int quality)
     {
-        
+        if (audioSource)
+        {
+            if (buttonClick)
+            {
+                audioSource.PlayOneShot(buttonClick);
+            } else
+            {
+                audioSource.Play();
+            }
+        }
+        QualitySettings.SetQualityLevel(quality);
     }
 }
