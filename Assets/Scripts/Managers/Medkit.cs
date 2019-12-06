bf047173b8b84bec8c321e95684a5818a0d84c50
Roopkotha Studio <roopkotha7d@gmail.com>
2019-12-06 09:33:20 +0600

    Created script

---
+++ Assets/Scripts/Managers/Medkit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Medkit : MonoBehaviour
+{
+    [SerializeField] private long heal = 15;
+    [SerializeField] private GameObject sound = null;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController && playerController.health > 0 && playerController.health < playerController.maxHealth)
+            {
+                playerController.health += heal;
+                if (sound) Instantiate(sound, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
